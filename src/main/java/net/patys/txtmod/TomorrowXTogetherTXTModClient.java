978d8387e26d273c62cd78bb6deb40365a51d0b9
RedStar3925 <pycbaterina@gmail.com>
2024-06-28 01:00:18 +0800

    first commit

---
+++ src/main/java/net/patys/txtmod/TomorrowXTogetherTXTModClient.java
@@ -0,0 +1,10 @@
+package net.patys.txtmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TomorrowXTogetherTXTModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
